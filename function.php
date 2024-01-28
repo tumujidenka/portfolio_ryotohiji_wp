43a7305a394138f9b0325fb8706e9ef5fb17052b
ryoto_hiji <ryotohiji.41@gmail.com>
2024-01-28 15:12:21 +0900

    CSS/JSが動作するようにリンクのパスを修正

--- function.php
+++ function.php
@@ -1,7 +1,8 @@
 <?php
 
-function my_enqueue_styles() {
-  wp_enqueue_style('destyle',get_template_directory_uri() . '/asset/css/destyle.css', array(), false, 'all');
-  wp_enqueue_style('style', get_template_directory_uri() . '/asset/css/style.css', array('destyle'), false, 'all');
-}
-add_action('wp_enqueue_scripts', 'my_enqueue_styles');
+// function my_enqueue_styles() {
+//   $theme_uri = get_template_directory_uri();
+//   wp_enqueue_style('destyle', $theme_uri . '/asset/css/destyle.css', array(), false, 'all');
+//   wp_enqueue_style('style', $theme_uri . '/asset/css/style.css', array('destyle'), false, 'all');
+// }
+// add_action('wp_enqueue_scripts', 'my_enqueue_styles');
