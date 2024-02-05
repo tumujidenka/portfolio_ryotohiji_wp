d3ac251c5e0b19042eda088054a6d3053746d99c
ryoto_hiji <ryotohiji.41@gmail.com>
2024-02-05 09:24:41 +0900

    ・archive.phpの作成

---
+++ archive.php
@@ -0,0 +1,153 @@
+<!-- Worksページ -->
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <?php get_header(); ?>
+</head>
+
+<body>
+    <?php get_template_part('includes/header'); ?>
+    <main>
+        <div class="works-child">
+            <div class="title">
+                <p>制作実績</p>
+                <h2>Works</h2>
+                <div></div>
+            </div>            
+            <div class="works-child__contents">
+                <a href="#">
+                    <div class="workspanel-child">
+                        <div class="workspanel-child__image">
+                            <img src="./asset/media/360_240.png" alt="site-image">
+                        </div>
+                        <div class="workspanel-child__plan">ライトプラン</div>
+                        <div class="workspanel-child__title">ストアサイト</div>
+                        <div class="workspanel-child__type">(Topページのみ)</div>
+                        <div class="workspanel-child__description">スニーカーを販売している会社のページになります。</div>
+                        <div class="workspanel-child__tech">使用技術：HTML/CSS/JavaScript</div>
+                        <div class="workspanel-child__period">制作期間：1週間</div>
+                    </div>    
+                </a>
+                <a href="#">
+                    <div class="workspanel-child">
+                        <div class="workspanel-child__image">
+                            <img src="./asset/media/360_240.png" alt="site-image">
+                        </div>
+                        <div class="workspanel-child__plan">ライトプラン</div>
+                        <div class="workspanel-child__title">ストアサイト</div>
+                        <div class="workspanel-child__type">(Topページのみ)</div>
+                        <div class="workspanel-child__description">スニーカーを販売している会社のページになります。</div>
+                        <div class="workspanel-child__tech">使用技術：HTML/CSS/JavaScript</div>
+                        <div class="workspanel-child__period">制作期間：1週間</div>
+                    </div>
+                </a>
+                <a href="#">
+                    <div class="workspanel-child">
+                        <div class="workspanel-child__image">
+                            <img src="./asset/media/360_240.png" alt="site-image">
+                        </div>
+                        <div class="workspanel-child__plan">ライトプラン</div>
+                        <div class="workspanel-child__title">ストアサイト</div>
+                        <div class="workspanel-child__type">(Topページのみ)</div>
+                        <div class="workspanel-child__description">スニーカーを販売している会社のページになります。</div>
+                        <div class="workspanel-child__tech">使用技術：HTML/CSS/JavaScript</div>
+                        <div class="workspanel-child__period">制作期間：1週間</div>
+                    </div>
+                </a>
+                <a href="#">
+                    <div class="workspanel-child">
+                        <div class="workspanel-child__image">
+                            <img src="./asset/media/360_240.png" alt="site-image">
+                        </div>
+                        <div class="workspanel-child__plan">ライトプラン</div>
+                        <div class="workspanel-child__title">ストアサイト</div>
+                        <div class="workspanel-child__type">(Topページのみ)</div>
+                        <div class="workspanel-child__description">スニーカーを販売している会社のページになります。</div>
+                        <div class="workspanel-child__tech">使用技術：HTML/CSS/JavaScript</div>
+                        <div class="workspanel-child__period">制作期間：1週間</div>
+                    </div>
+                </a>
+                <a href="#">
+                    <div class="workspanel-child">
+                        <div class="workspanel-child__image">
+                            <img src="./asset/media/360_240.png" alt="site-image">
+                        </div>
+                        <div class="workspanel-child__plan">ライトプラン</div>
+                        <div class="workspanel-child__title">ストアサイト</div>
+                        <div class="workspanel-child__type">(Topページのみ)</div>
+                        <div class="workspanel-child__description">スニーカーを販売している会社のページになります。</div>
+                        <div class="workspanel-child__tech">使用技術：HTML/CSS/JavaScript</div>
+                        <div class="workspanel-child__period">制作期間：1週間</div>
+                    </div>
+                </a>
+                <a href="#">
+                    <div class="workspanel-child">
+                        <div class="workspanel-child__image">
+                            <img src="./asset/media/360_240.png" alt="site-image">
+                        </div>
+                        <div class="workspanel-child__plan">ライトプラン</div>
+                        <div class="workspanel-child__title">ストアサイト</div>
+                        <div class="workspanel-child__type">(Topページのみ)</div>
+                        <div class="workspanel-child__description">スニーカーを販売している会社のページになります。</div>
+                        <div class="workspanel-child__tech">使用技術：HTML/CSS/JavaScript</div>
+                        <div class="workspanel-child__period">制作期間：1週間</div>
+                    </div>
+                </a>
+                <a href="#">
+                    <div class="workspanel-child">
+                        <div class="workspanel-child__image">
+                            <img src="./asset/media/360_240.png" alt="site-image">
+                        </div>
+                        <div class="workspanel-child__plan">ライトプラン</div>
+                        <div class="workspanel-child__title">ストアサイト</div>
+                        <div class="workspanel-child__type">(Topページのみ)</div>
+                        <div class="workspanel-child__description">スニーカーを販売している会社のページになります。</div>
+                        <div class="workspanel-child__tech">使用技術：HTML/CSS/JavaScript</div>
+                        <div class="workspanel-child__period">制作期間：1週間</div>
+                    </div>
+                </a>
+                <a href="#">
+                    <div class="workspanel-child">
+                        <div class="workspanel-child__image">
+                            <img src="./asset/media/360_240.png" alt="site-image">
+                        </div>
+                        <div class="workspanel-child__plan">ライトプラン</div>
+                        <div class="workspanel-child__title">ストアサイト</div>
+                        <div class="workspanel-child__type">(Topページのみ)</div>
+                        <div class="workspanel-child__description">スニーカーを販売している会社のページになります。</div>
+                        <div class="workspanel-child__tech">使用技術：HTML/CSS/JavaScript</div>
+                        <div class="workspanel-child__period">制作期間：1週間</div>
+                    </div>
+                </a>
+                <a href="#">
+                    <div class="workspanel-child">
+                        <div class="workspanel-child__image">
+                            <img src="./asset/media/360_240.png" alt="site-image">
+                        </div>
+                        <div class="workspanel-child__plan">ライトプラン</div>
+                        <div class="workspanel-child__title">ストアサイト</div>
+                        <div class="workspanel-child__type">(Topページのみ)</div>
+                        <div class="workspanel-child__description">スニーカーを販売している会社のページになります。</div>
+                        <div class="workspanel-child__tech">使用技術：HTML/CSS/JavaScript</div>
+                        <div class="workspanel-child__period">制作期間：1週間</div>
+                    </div>
+                </a>
+            </div>
+            <div class="pagination">
+                <div>
+                    <a href="#"  class="pagination__number active">1</a>
+                </div>
+                <div>
+                   <a href="#" class="pagination__number">2</a>
+                </div>
+                <div>
+                   <a href="#" class="pagination__number">3</a>
+                </div>
+            </div>
+        </div>
+    </main>
+    <?php get_template_part('includes/footer'); ?>
+
+    <?php get_footer(); ?>
+</body>
+</html>
